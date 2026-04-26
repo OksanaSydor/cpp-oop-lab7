41c47f43fddadfdc4d39a162c91d2844050fc13a
OksanaSydor <sydor.oksana1111@gmail.com>
2026-04-26 20:42:42 +0300

    Implement function for range insertion in vectors

--- lab7/Lab8/main.cpp
+++ lab7/Lab8/main.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include <array>
+#include <vector>
 #include <string>
 #include "Student.h"
 
 using namespace std;
 
+// Task 1
 template <typename T, size_t size>
 void printArray(const array<T, size> &arr)
 {
@@ -51,8 +53,52 @@ void shiftClockwise(array<T, size> &arr, int shift)
   }
 }
 
+// Task 2
+
+template <typename T>
+void printVector(const vector<T> &vector)
+{
+  for (auto it = vector.begin(); it != vector.end(); ++it)
+  {
+    cout << *it << " ";
+  }
+  cout << endl;
+}
+
+template <typename T>
+void insertRange(vector<T> &first, const vector<T> &second, int position)
+{
+  if (position < 0)
+  {
+    position = 0;
+  }
+
+  if (position > first.size())
+  {
+    position = first.size();
+  }
+
+  auto insertIt = first.begin();
+
+  for (int i = 0; i < position; i++)
+  {
+    ++insertIt;
+  }
+
+  auto secondIt = second.begin();
+
+  for (; secondIt != second.end(); ++secondIt)
+  {
+    insertIt = first.insert(insertIt, *secondIt);
+    ++insertIt;
+  }
+}
+
 int main()
 {
+  cout << "---------- Task 1 ----------" << endl;
+  cout << "\n";
+
   array<int, 5> a1 = {1, 2, 3, 4, 5};
 
   cout << "Before: ";
@@ -76,7 +122,7 @@ int main()
   cout << "\n";
 
   array<Student, 3> a3 = {
-      Student("Oksana", 99),
+      Student("Arsen", 99),
       Student("Ira", 88),
       Student("Nastia", 85)};
 
@@ -87,6 +133,51 @@ int main()
 
   cout << "After: ";
   printArray(a3);
+  cout << "\n";
+
+  cout << "---------- Task 2 ----------" << endl;
+  cout << "\n";
+
+  vector<int> v1 = {1, 2, 3, 4, 5};
+  vector<int> v2 = {10, 200, 3000};
+
+  cout << "Before: ";
+  printVector(v1);
+
+  insertRange(v1, v2, 3);
+
+  cout << "After: ";
+  printVector(v1);
+  cout << "\n";
+
+  vector<string> s1 = {"Apple", "Tomato", "Cucumber"};
+  vector<string> s2 = {"sun", "moon"};
+
+  cout << "Before: ";
+  printVector(s1);
+
+  insertRange(s1, s2, 1);
+
+  cout << "After: ";
+  printVector(s1);
+  cout << "\n";
+
+  vector<Student> vst1 = {
+      Student("Arsen", 99),
+      Student("Ira", 88)};
+
+  vector<Student> vst2 = {
+      Student("Nastia", 85),
+      Student("Marta", 91)};
+
+  cout << "Before: ";
+  printVector(vst1);
+
+  insertRange(vst1, vst2, 10);
+
+  cout << "After: ";
+  printVector(vst1);
+  cout << "\n";
 
   return 0;
 }
