365a0223163c2aafe5793edd3e223bfc5629b973
OksanaSydor <sydor.oksana1111@gmail.com>
2026-05-03 16:26:35 +0300

    Add array sorting

--- lab7/Lab9/main9.cpp
+++ lab7/Lab9/main9.cpp
@@ -3,9 +3,12 @@
 #include <list>
 #include <cstdlib>
 #include <ctime>
+#include <array>
+#include <string>
 
 using namespace std;
 
+// Task 1
 bool isOdd(int n)
 {
   return n % 2 != 0;
@@ -21,8 +24,26 @@ void printList(const list<T> &l)
   cout << '\n';
 }
 
+// Task 2
+bool compareLength(const string &a, const string &b)
+{
+  return a.length() < b.length();
+}
+
+template <typename T, size_t size>
+void printArray(const array<T, size> &arr)
+{
+  for (const T &value : arr)
+  {
+    cout << value << " ";
+  }
+  cout << '\n';
+}
+
 int main()
 {
+  cout << "----------Task 1----------" << endl;
+
   list<int> numbers;
 
   srand(time(nullptr));
@@ -40,6 +61,18 @@ int main()
 
   cout << "After: \n";
   printList(numbers);
+
+  cout << "----------Task 2----------" << endl;
+
+  array<string, 6> names = {"Oksana", "Ivan", "Anna", "Petro", "Olha", "Max"};
+
+  cout << "Before: \n";
+  printArray(names);
+
+  sort(names.begin(), names.end(), compareLength);
+
+  cout << "After: \n";
+  printArray(names);
 
   return 0;
 }
